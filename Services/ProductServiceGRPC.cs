01d022279d3b29f0cf9b99c70b4be5ba35881b7f
frmauro <frmauro8@gmail.com>
2021-12-09 11:13:35 -0300

    many changes

--- Services/ProductServiceGRPC.cs
+++ Services/ProductServiceGRPC.cs
@@ -1,7 +1,29 @@
+using Grpc.Core;
+
 namespace ApiGetwaySalesOrder.Services
 {
-    public class ProductServiceGRPC
+    public class ProductServiceGRPC : SalesProductApi.ProductServiceProto.ProductServiceProtoBase
     {
-        public ProductServiceGRPC() { }
+                public override Task<SalesProductApi.ItemResponse> GetProducts(SalesProductApi.Empty request, ServerCallContext _context){
+
+            List<SalesProductApi.ProductResponse> products = new List<SalesProductApi.ProductResponse>();
+                    SalesProductApi.ItemResponse response = new SalesProductApi.ItemResponse();
+
+                    //var productsDb = _context.Products.ToList();
+
+                    // productsDb.ToList().ForEach(productDb => {
+                    //     var product = new SalesProductApi.ProductResponse();
+                    //     product.Id = productDb.ProductId;
+                    //     product.Description = productDb.Description;
+                    //     product.Amount = productDb.Amount.ToString();
+                    //     product.Price = productDb.Price.ToString();
+                    //     product.Status = productDb.Status.ToString();
+                    //     products.Add(product);
+                    // });
+
+                    response.Items.AddRange(products);
+                    return Task.FromResult(response);
+
+        }
     }
 }
